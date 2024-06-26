df11df59e25b4b68482b9ec774fc45fc61859344
Aryan <145794148+aryanshaikh483@users.noreply.github.com>
2024-06-26 16:19:19 +0530

    feat[EditTopic,editTopic/[id]/page.jsx]:add put method to edit topic feature

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,17 @@
 "use client";
 
 import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button"
+
 
 export default function Navbar(){
     return(
         <nav className= "flex justify-between items-center bg-slate-800 px-8 py-3">
             <Link className="text-white font-bold" href={'/'}>Today You Do</Link>
-            <Link className="bg-white p-2" href={'/addTopic'}>Add Topic</Link>
+            {/* <Link className="bg-white p-2" href={'/addTopic'}>Add Topic</Link> */}
+            <Link className={buttonVariants({ variant: "outline" })} href={'/addTopic'}>Add Topic</Link>
+            
 
         </nav>
     )
